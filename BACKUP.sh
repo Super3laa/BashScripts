fe7a2c5b25e3173bdf161f8dac9d3b72ef858958
Alaa Essam <superalaa28@gmail.com>
2020-11-13 13:30:41 +0200

    Update BACKUP.sh

--- BACKUP.sh
+++ BACKUP.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 DATEVAR=`date +20\%y\%m\%d_\%H\%M\%S`
-sudo mysqldump -u root --password="password" drivingSchool > /root/BACKUPS/CAbackup${DATEVAR}.sql && ./Dropbox-Uploader/dropbox_uploader.sh upload ./BACKUPS/CAbackup${DATEVAR}.sql /CAbackup${DATEVAR}.sql
+sudo mysqldump -u root -p drivingSchool > /root/BACKUPS/CAbackup${DATEVAR}.sql && ./Dropbox-Uploader/dropbox_uploader.sh upload ./BACKUPS/CAbackup${DATEVAR}.sql /CAbackup${DATEVAR}.sql
